6bf7b4f05ea90f63aff4ebba7582b7532e8531ea
Karl Clement <karlclement@gmail.com>
2011-08-02 17:08:27 -0400

    Cleared database data

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/~karlclement/womti.com/';
+$config['base_url']	= 'http://localhost/';
 
 /*
 |--------------------------------------------------------------------------
