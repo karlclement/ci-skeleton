52838e85c6de971386c422765996cd5d9a0e3822
Karl Clement <karlclement@gmail.com>
2011-08-22 23:20:29 -0400

    Changed links in head.php

--- application/views/head.php
+++ application/views/head.php
@@ -28,10 +28,10 @@
 	
 	<!-- Favicons
 	================================================== -->
-	<link rel="shortcut icon" href="<? base_url(); ?>/include/images/favicon.ico">
-	<link rel="apple-touch-icon" href="<? base_url(); ?>/include/images/apple-touch-icon.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="<? base_url(); ?>/include/images/apple-touch-icon-72x72.png" />
-	<link rel="apple-touch-icon" sizes="114x114" href="<? base_url(); ?>/include/images/apple-touch-icon-114x114.png" />
+	<link rel="shortcut icon" href="<? base_url(); ?>include/images/favicon.ico">
+	<link rel="apple-touch-icon" href="<? base_url(); ?>include/images/apple-touch-icon.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="<? base_url(); ?>include/images/apple-touch-icon-72x72.png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="<? base_url(); ?>include/images/apple-touch-icon-114x114.png" />
 	
 </head>
 <body>
